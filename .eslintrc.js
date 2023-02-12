e863c90254777d269969d634969e8bbd134ac9ca
Vlad Lebedev <vladlebedeff@gmail.com>
2023-02-12 15:13:47 +0200

    2023-01-27 savepoint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+/***
+ * Excerpted from "Modern Front-End Development for Rails, Second Edition",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit https://pragprog.com/titles/nrclient2 for more book information.
+***/
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+    "plugin:cypress/recommended",
+  ],
+  parser: "@typescript/eslint-parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: "@typescript/eslint-parser",
+    requireConfigFile: false,
+    sourceType: "module",
+  },
+  rules: {},
+  globals: {
+    require: true,
+    Stimulus: true,
+    process: true,
+    module: true,
+  },
+};
---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,31 @@
+#---
+# Excerpted from "Modern Front-End Development for Rails, Second Edition",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit https://pragprog.com/titles/nrclient2 for more book information.
+#---
+class FavoritesController < ApplicationController
+  def create
+    @favorite = Favorite.create(user: current_user, concert_id: params[:concert_id])
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    @favorite.destroy
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+
+  def index
+  end
+
+  private def favorite_params
+    params.require(:concert_id)
+  end
+end
